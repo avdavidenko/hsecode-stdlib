427ef6e51a5292e5c8d33599a00a4af528b14f4f
Anastasia Davidenko <davidenkonastya98@gmail.com>
2020-06-15 14:20:39 +0300

    Add files via upload

---
+++ cmp/cmp.go
@@ -0,0 +1,32 @@
+package cmp
+
+// good IDEs will put the import statement automatically
+import "github.com/cheekybits/genny/generic"
+
+type ValueType generic.Number
+
+func Max(values ...ValueType) ValueType {
+	if len(values) == 0 {
+		panic("at least one argument required")
+	}
+	m := values[0]
+	for i := 1; i < len(values); i++ {
+		if values[i] > m {
+			m = values[i]
+		}
+	}
+	return m
+}
+
+func Min(values ...ValueType) ValueType {
+	if len(values) == 0 {
+		panic("at least one argument required")
+	}
+	m := values[0]
+	for i := 1; i < len(values); i++ {
+		if values[i] < m {
+			m = values[i]
+		}
+	}
+	return m
+}
