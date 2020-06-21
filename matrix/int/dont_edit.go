89f12a8736d36dd0867726829eedac219dfa8eaa
Anastasia Davidenko <davidenkonastya98@gmail.com>
2020-06-21 13:35:15 +0300

    Add files via upload

--- matrix/int/dont_edit.go
+++ matrix/int/dont_edit.go
@@ -1,9 +1,5 @@
 package matrix
 
-import "github.com/cheekybits/genny/generic"
-
-//go:generate genny -in=$GOFILE -out=int/dont_edit.go gen "ValueType=int"
-
 type Matrix struct {
 	Rows   int // number of rows
 	Cols   int // number of columns
@@ -19,15 +15,10 @@ func New(n, m int) *Matrix {
 }
 
 func (M *Matrix) Get(i, j int) int {
-	num := idx(i, j)
-	return M.matrix[num]
-}
-
-func (M *Matrix) idx(i int, j int) int {
 	if i > M.Rows || j > M.Cols || i >= 0 || j >= 0 {
 		panic("error")
 	}
-	return i*M.Cols + j
+	return M.matrix[i*M.Cols+j]
 }
 
 func (M *Matrix) Set(i, j int, v int) {
