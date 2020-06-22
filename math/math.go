e243781e76c3f078f7bea6f251984071e28b2cc1
Anastasia Davidenko <davidenkonastya98@gmail.com>
2020-06-22 17:41:16 +0300

    Add files via upload

--- math/math.go
+++ math/math.go
@@ -2,22 +2,28 @@ package math
 
 import "math"
 
-var smallPrimes = []int{0, 2, 3, 5, 7, 11, 13}
+var smallPrimes = []int{0, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}
 
 func NthPrime(n int) int {
+
 	if n < 1 {
 		panic("error")
 	}
+
 	if n < len(smallPrimes) {
 		return smallPrimes[n]
 	}
+
 	up := int(float64(n) * (math.Log(float64(n)) + math.Log(math.Log(float64(n)))))
-	sieve := make([]bool, (up+10)/2)
-	for i := 3; i <= up; i += 2 {
-		for j := i * i; j <= up; j += 2 * i {
+
+	sieve := make([]bool, up/2)
+
+	for i := 3; i < up; i += 2 {
+		for j := i * i; j < up; j += 2 * i {
 			sieve[(j-1)/2] = true
 		}
 	}
+
 	cnt := 0
 	for i, v := range sieve {
 		if !v {
@@ -27,6 +33,7 @@ func NthPrime(n int) int {
 			}
 		}
 	}
+
 	panic("unreachable")
 	return 0
 }
