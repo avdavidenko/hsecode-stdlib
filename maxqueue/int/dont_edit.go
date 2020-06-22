8643fbd5b8dbae204af19d366a1540db7370b372
Anastasia Davidenko <davidenkonastya98@gmail.com>
2020-06-23 00:12:42 +0300

    Add files via upload

--- maxqueue/int/dont_edit.go
+++ maxqueue/int/dont_edit.go
@@ -1,11 +1,12 @@
 package MaxQueue
 
-import "container/list"
 import "errors"
 
 type MaxQueue struct {
-	elements list.List
-	maxes    list.List
+	elements_in []int
+	maxes_in    []int
+	elements_ou []int
+	maxes_ou    []int
 }
 
 func New() *MaxQueue {
@@ -13,35 +14,61 @@ func New() *MaxQueue {
 }
 
 func (q *MaxQueue) Max() (int, error) {
-	if q.elements.Len() == 0 {
-		return 0, errors.New("Queue is empty")
+	if len(q.maxes_ou) > 0 && len(q.maxes_in) > 0 {
+		if q.maxes_ou[len(q.maxes_ou)-1] > q.maxes_in[len(q.maxes_in)-1] {
+			return q.maxes_ou[len(q.maxes_ou)-1], nil
+		} else {
+			return q.maxes_in[len(q.maxes_in)-1], nil
+		}
 	}
-	return q.maxes.Front().Value.(int), nil
+
+	if len(q.maxes_ou) > 0 {
+		return q.maxes_ou[len(q.maxes_ou)-1], nil
+	}
+
+	if len(q.maxes_in) > 0 {
+		return q.maxes_in[len(q.maxes_in)-1], nil
+	}
+
+	return 0, errors.New("Queue is empty")
 }
 
 func (q *MaxQueue) Pop() (int, error) {
-	if q.elements.Len() == 0 {
+	if len(q.maxes_ou) == 0 {
+		for i := len(q.maxes_in) - 1; i >= 0; i-- {
+			value := q.elements_in[i]
+			q.elements_ou = append(q.elements_ou, value)
+			max := value
+			if len(q.maxes_ou) > 0 && q.maxes_ou[len(q.maxes_ou)-1] > value {
+				max = q.maxes_ou[len(q.maxes_ou)-1]
+			}
+			q.maxes_ou = append(q.maxes_ou, max)
+		}
+		q.maxes_in = q.maxes_in[:0]
+		q.elements_in = q.elements_in[:0]
+	}
+
+	if len(q.maxes_ou) == 0 {
 		return 0, errors.New("Queue is empty")
 	}
 
-	temp := q.elements.Front()
-	value := temp.Value.(int)
-	q.elements.Remove(temp)
+	q.maxes_ou = q.maxes_ou[:len(q.maxes_ou)-1]
+	value := q.elements_ou[len(q.elements_ou)-1]
+	q.elements_ou = q.elements_ou[:len(q.elements_ou)-1]
 
-	if value == q.maxes.Front().Value {
-		q.maxes.Remove(q.maxes.Front())
-	}
 	return value, nil
 }
 
 func (q *MaxQueue) Push(value int) {
-	q.elements.PushBack(value)
-	if q.maxes.Len() == 0 {
-		q.maxes.PushBack(value)
+	q.elements_in = append(q.elements_in, value)
+	if len(q.maxes_in) == 0 {
+		q.maxes_in = append(q.maxes_in, value)
 	} else {
-		for q.maxes.Len() > 0 && q.maxes.Back().Value.(int) < value {
-			q.maxes.Remove(q.maxes.Back())
+		max := q.maxes_in[len(q.maxes_in)-1]
+		if max > value {
+			q.maxes_in = append(q.maxes_in, max)
+		} else {
+			q.maxes_in = append(q.maxes_in, value)
 		}
-		q.maxes.PushBack(value)
 	}
 }
