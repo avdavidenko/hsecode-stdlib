f7aa7be03b253efeeb6b240adce722113e9378d6
Anastasia Davidenko <davidenkonastya98@gmail.com>
2020-06-23 21:34:30 +0300

    Add files via upload

---
+++ pick/firstN.go
@@ -0,0 +1,61 @@
+package pick
+
+import (
+	"container/heap"
+	"sort"
+)
+
+type Ordered interface {
+	Len() int
+	Less(i, j int) bool
+	Swap(i, j int)
+}
+
+type Heap struct {
+	indices []int
+	data    Ordered
+}
+
+func (h *Heap) Len() int {
+	return len(h.indices)
+}
+
+func (h *Heap) Swap(i, j int) {
+	h.indices[i], h.indices[j] = h.indices[j], h.indices[i]
+}
+
+func (h *Heap) Less(i, j int) bool {
+	return h.data.Less(h.indices[i], h.indices[j])
+}
+
+func (h *Heap) Push(x interface{}) {
+	h.indices = append(h.indices, x.(int))
+}
+
+func (h *Heap) Pop() interface{} {
+	old := h.indices
+	n := len(old)
+	x := old[n-1]
+	h.indices = old[0 : n-1]
+	return x
+}
+
+func FirstN(data Ordered, n int) []int {
+	fmt.Println(data)
+
+	h := &Heap{data: sort.Reverse(data)}
+	heap.Init(h)
+
+	for i := 0; i < data.Len(); i++ {
+		if i >= n {
+			if h.data.Less(h.indices[0], i) {
+				heap.Pop(h)
+				heap.Push(h, i)
+			}
+		} else {
+			heap.Push(h, i)
+		}
+	}
+
+	return h.indices
+}
