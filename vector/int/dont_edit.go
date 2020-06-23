2d1cfc8d04401724cfd7e54a440d54ad7c4949ba
Anastasia Davidenko <davidenkonastya98@gmail.com>
2020-06-23 09:49:19 +0300

    Add files via upload

--- vector/int/dont_edit.go
+++ vector/int/dont_edit.go
@@ -1,6 +1,7 @@
 package vector
 
 type Vector struct {
+	Len    int
 	vector []int
 }
 
@@ -9,75 +10,76 @@ func New(cap int) *Vector {
 	if newCap < 0 {
 		newCap = 0
 	}
-	return &Vector{vector: make([]int, 0, newCap)}
+	return &Vector{vector: make([]int, newCap), Len: 0}
 }
 
 func (a *Vector) Push(x int) {
-	a.Insert(len(a.vector), x)
+	a.Insert(a.Len, x)
 }
 
 func (a *Vector) Pop() int {
-	m := a.Get(len(a.vector) - 1)
-	a.Delete(len(a.vector) - 1)
+	m := a.Get(a.Len - 1)
+	a.Delete(a.Len - 1)
 	return m
 }
 
 func (a *Vector) Set(idx int, x int) {
-	if idx < 0 || idx >= len(a.vector) {
+	if idx < 0 || idx >= a.Len {
 		panic("Index out of range")
 	}
 	a.vector[idx] = x
 }
 
 func (a *Vector) Get(idx int) int {
-	if idx < 0 || idx >= len(a.vector) {
+	if idx < 0 || idx >= a.Len {
 		panic("Index out of range")
 	}
 	return a.vector[idx]
 }
 
 func (a *Vector) Insert(idx int, x int) {
-	if idx < 0 || idx > len(a.vector) {
+	if idx < 0 || idx > a.Len {
 		panic("Index out of range")
 	}
 
-	if len(a.vector) == cap(a.vector) {
-		newCap := len(a.vector) * 2
+	if a.Len == len(a.vector) {
+		newCap := a.Len * 2
 		if newCap <= 0 {
 			newCap = 8
 		}
-		newVector := make([]int, len(a.vector), newCap)
+		newVector := make([]int, newCap)
 		copy(newVector, a.vector)
 		a.vector = newVector
 	}
 
-	if idx == len(a.vector) {
-		a.vector = append(a.vector, x)
+	if idx == a.Len {
+		a.vector[idx] = x
 	} else {
-		a.vector = append(a.vector, a.vector[len(a.vector)-1])
-		for i := len(a.vector) - 1; i > idx; i-- {
+		for i := a.Len; i > idx; i-- {
 			a.vector[i] = a.vector[i-1]
 		}
 		a.vector[idx] = x
 	}
+
+	a.Len++
 }
 
 func (a *Vector) Delete(idx int) {
-	if idx < 0 || idx >= len(a.vector) {
+	if idx < 0 || idx >= a.Len {
 		panic("Index out of range")
 	}
 
-	if len(a.vector) == 0 {
+	if a.Len == 0 {
 		panic("Vector is empty")
 	}
 
-	for i := idx + 1; i < len(a.vector); i++ {
+	for i := idx + 1; i < a.Len; i++ {
 		a.vector[i-1] = a.vector[i]
 	}
-	a.vector = a.vector[:len(a.vector)-1]
+	a.Len--
 
-	if len(a.vector) <= cap(a.vector)/4 {
-		newVector := make([]int, len(a.vector), cap(a.vector)/2)
+	if a.Len <= len(a.vector)/4 {
+		newVector := make([]int, len(a.vector)/2)
 		copy(newVector, a.vector)
 		a.vector = newVector
 	}
