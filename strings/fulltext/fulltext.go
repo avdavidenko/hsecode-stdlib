62bdea6344bb2cc07bb6b1d26bdfa40a5fa8bf2e
Anastasia Davidenko <davidenkonastya98@gmail.com>
2020-06-27 13:26:40 +0300

    Add files via upload

--- strings/fulltext/fulltext.go
+++ strings/fulltext/fulltext.go
@@ -3,7 +3,7 @@ package fulltext
 import "strings"
 
 type Index struct {
-	idx map[string]([]int)
+	idx        map[string]([]int)
 	docsNumber int
 }
 
@@ -14,7 +14,7 @@ func New(docs []string) *Index {
 	for i := 0; i < idx.docsNumber; i++ {
 		idx.addDoc(i, docs[i])
 	}
-	
+
 	return idx
 }
 
@@ -22,32 +22,32 @@ func getWord(idx int, doc string) (string, int) {
 	for idx < len(doc) && doc[idx] == ' ' {
 		idx++
 	}
-	
+
 	startIdx := idx
 	for idx < len(doc) && doc[idx] != ' ' {
 		idx++
 	}
-	
-	return doc[startIdx : idx], idx
+
+	return doc[startIdx:idx], idx
 }
 
-func (idx *Index) addDoc(number int, doc string){
+func (idx *Index) addDoc(number int, doc string) {
 	i := 0
 	word := ""
 	for {
-		word, i = getWord(i, doc); 
+		word, i = getWord(i, doc)
 		if len(word) == 0 {
 			break
 		}
 
-		 docIdxes, ok := idx.idx[word]
-		 if ok {
-			if docIdxes[len(docIdxes) - 1] != number {
+		docIdxes, ok := idx.idx[word]
+		if ok {
+			if docIdxes[len(docIdxes)-1] != number {
 				idx.idx[word] = append(docIdxes, number)
 			}
-		 } else {
+		} else {
 			idx.idx[word] = []int{number}
-		 }
+		}
 	}
 }
 
@@ -55,14 +55,14 @@ func (idx *Index) Search(query string) []int {
 	if len(query) == 0 {
 		return []int{}
 	}
-	
+
 	words := strings.Split(query, " ")
 	if len(words) == 0 {
 		return []int{}
 	}
 
 	meets := make([]int, idx.docsNumber)
-	
+
 	for i := 0; i < len(words); i++ {
 		docIdxes, ok := idx.idx[words[i]]
 		if ok {
@@ -71,13 +71,13 @@ func (idx *Index) Search(query string) []int {
 			}
 		}
 	}
-	
+
 	result := make([]int, 0)
 	for i := 0; i < idx.docsNumber; i++ {
 		if meets[i] == len(words) {
-		result = append(result, i)
+			result = append(result, i)
 		}
 	}
-	
+
 	return result
 }
